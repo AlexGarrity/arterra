22552846295fee470c9d831abae7714cb0e85446
AlexGarrity <garrity2799@gmail.com>
2020-06-29 00:43:32 +0100

    Test file

--- arterra/src/Main.cpp
+++ arterra/src/Main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
-int main() {
+#include <gl/gl3w.h>
+#include <glfw/glfw3.h>
+#include <glm/glm.hpp>
+
+int main()
+{
+    glm::vec3 {1, 2, 3};
+    gl3wInit();
+    glfwInit();
     std::cout << "Hello world" << std::endl;
     return 0;
 }
