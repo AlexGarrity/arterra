a022822908580c86608d618bdcdcd46420dd1447
AlexGarrity <garrity2799@gmail.com>
2020-06-29 14:35:14 +0100

    Some window code

--- arterra/src/Main.cpp
+++ arterra/src/Main.cpp
@@ -5,12 +5,41 @@
 #include <glm/glm.hpp>
 
 int main()
-{
-    glm::vec3 vector {1.0f, 2.0f, 3.0f};
-    auto x = vector.length();
+{   
+    if (!glfwInit()) {
+        std::cerr << "GLFW failed to initialise" << std::endl;
+        return -1;
+    }
 
-    gladLoadGL();
-    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "Window", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    if (!gladLoadGL()) {
+        std::cerr << "GLAD failed to initialise" << std::endl;
+        return -1;
+    }
+
+    gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+
+    while (!glfwWindowShouldClose(window)) {
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+        }
+    }
 
     std::cout << "Hello world" << std::endl;
     return 0;
