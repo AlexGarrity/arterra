fbabbb594ac13066fbdc88ca7d956aa2260356bf
AlexGarrity <garrity2799@gmail.com>
2020-06-29 15:33:14 +0100

    Pre-compiled header

---
+++ arterra/include/PCH.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+// Console IO
+#include <iostream>
+// Strings
+#include <string>
+// File IO
+#include <fstream>
+// String Streams
+#include <sstream>
+
+// Modern C++ Ptrs
+#include <memory>
+// Threading
+#include <thread>
+// Mutexes (thread locks)
+#include <mutex>
+
+// STL Algorithms
+#include <algorithm>
+// Function ptrs
+#include <functional>
+// Maths functions
+#include <cmath>
+// Time functions
+#include <chrono>
+// Random functions
+#include <random>
+
+// Lists
+#include <vector>
+// Arrays
+#include <array>
+// Maps / Dicts
+#include <map>
+#include <unordered_map>
+// Sets (lists w/o duplication)
+#include <set>
+#include <unordered_set>
