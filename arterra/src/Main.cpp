aa656d24a56fa398ff0bc09ba6350512cfa14308
Alex Garrity <garrity2799@gmail.com>
2020-06-29 04:34:14 +0100

    Commented a lot of things (and made sure it compiles on Linux)

--- arterra/src/Main.cpp
+++ arterra/src/Main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
- 
-#include <gl/gl3w.h>
-#include <glfw/glfw3.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
 int main()
 {
-    glm::vec3 {1, 2, 3};
-    gl3wInit();
+    glm::vec3 vector {1.0f, 2.0f, 3.0f};
+    auto x = vector.length();
+
+    gladLoadGL();
     glfwInit();
+
     std::cout << "Hello world" << std::endl;
     return 0;
 }
