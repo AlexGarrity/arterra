bbf4e99bb00ed0961f8b15dfa2dcb3bacd9d0e13
Alex Garrity <garrity2799@gmail.com>
2021-05-10 11:03:54 +0100

    Fixed positive value on PMod

--- arterra/include/util/Maths.hpp
+++ arterra/include/util/Maths.hpp
@@ -6,7 +6,7 @@ namespace arterra {
 
 	namespace Maths {
 
-		unsigned PMod(unsigned value, unsigned m)
+		unsigned PMod(int value, unsigned m)
 		{
 			int r = value % m;
 			return (r < 0) ? (r + m) : r;
